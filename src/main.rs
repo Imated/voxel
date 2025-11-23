b63f38844642854a3f31db198897a64f0b169fb2
Imated <lineborder122@gmail.com>
2025-11-23 14:43:26 -0500

    Setup WGPU, Clear window color with render pass

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ mod macros;
 use log::*;
 use std::process::abort;
 use std::rc::Rc;
+use std::sync::Arc;
+use wgpu::PresentMode::Mailbox;
+use wgpu::wgt::CommandEncoderDescriptor;
+use wgpu::{
+    Backends, Color, Device, DeviceDescriptor, Features, Instance, InstanceDescriptor, Limits,
+    LoadOp, Operations, PresentMode, Queue, RenderPassColorAttachment, RenderPassDescriptor,
+    RequestAdapterOptions, StoreOp, Surface, SurfaceConfiguration, TextureUsages,
+    TextureViewDescriptor, Trace,
+};
 use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::{KeyEvent, WindowEvent};
@@ -11,20 +20,143 @@ use winit::keyboard::{KeyCode, PhysicalKey};
 use winit::window::{Window, WindowId};
 
 pub struct State {
-    window: Rc<Window>,
+    surface: Surface<'static>,
+    device: Device,
+    queue: Queue,
+    config: SurfaceConfiguration,
+    is_surface_configured: bool,
+    window: Arc<Window>,
 }
 
 impl State {
-    pub async fn new(window: Rc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        let size = window.inner_size();
+
+        let instance = Instance::new(&InstanceDescriptor {
+            backends: Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window.clone())?;
+
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                power_preference: Default::default(),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await?;
+
+        let (device, queue) = adapter
+            .request_device(&DeviceDescriptor {
+                label: None,
+                required_features: Features::empty(),
+                required_limits: Limits::default(),
+                memory_hints: Default::default(),
+                trace: Trace::Off,
+            })
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|format| format.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+        assert_ne!(size.width, 0, "Window width or height is zero!");
+        assert_ne!(size.height, 0, "Window width or height is zero!");
+        let config = SurfaceConfiguration {
+            usage: TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps
+                .present_modes
+                .contains(&Mailbox)
+                .then(|| Mailbox)
+                .unwrap_or(PresentMode::Fifo),
+            desired_maximum_frame_latency: 2,
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+            window,
+        })
     }
 
     pub fn resize(&mut self, width: u32, height: u32) {
-
+        if width <= 0 && height <= 0 {
+            return;
+        }
+        self.config.width = width;
+        self.config.height = height;
+        self.surface.configure(&self.device, &self.config);
+        self.is_surface_configured = true;
     }
 
-    pub fn render(&mut self) {
+    pub fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+        match (code, is_pressed) {
+            (KeyCode::Escape, true) => event_loop.exit(),
+            _ => {}
+        }
+    }
+
+    pub fn update(&mut self) {}
+
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         self.window.request_redraw();
+
+        if !self.is_surface_configured {
+            return Ok(());
+        }
+
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&CommandEncoderDescriptor {
+                // cmd buffer in disguise!
+                label: Some("Render Encoder"),
+            });
+
+        let render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(RenderPassColorAttachment {
+                view: &view,
+                depth_slice: None,
+                resolve_target: None,
+                ops: Operations {
+                    load: LoadOp::Clear(Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+
+        drop(render_pass);
+
+        self.queue.submit([encoder.finish()]);
+        self.window.pre_present_notify();
+        output.present();
+
+        Ok(())
     }
 }
 
@@ -34,19 +166,17 @@ struct App {
 
 impl App {
     pub fn new(event_loop: &EventLoop<()>) -> Self {
-        Self {
-            state: None,
-        }
+        Self { state: None }
     }
 }
 
-impl ApplicationHandler<> for App {
+impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let window_attributes = Window::default_attributes()
             .with_title("Voxel Engine")
             .with_inner_size(LogicalSize::new(800, 600))
             .with_resizable(true);
-        let window = Rc::new(
+        let window = Arc::new(
             event_loop
                 .create_window(window_attributes)
                 .unwrap_or_else(|err| fatal!("Failed to create window! Error: {:?}", err)),
@@ -70,7 +200,11 @@ impl ApplicationHandler<> for App {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::Resized(size) => state.resize(size.width, size.height),
-            WindowEvent::RedrawRequested => state.render(),
+            WindowEvent::RedrawRequested => {
+                state
+                    .render()
+                    .unwrap_or_else(|err| error!("Failed to render! Error: {:?}", err));
+            }
             WindowEvent::KeyboardInput {
                 event:
                     KeyEvent {
@@ -79,10 +213,7 @@ impl ApplicationHandler<> for App {
                         ..
                     },
                 ..
-            } => match (code, key_state.is_pressed()) {
-                (KeyCode::Escape, true) => event_loop.exit(),
-                _ => {}
-            },
+            } => state.handle_key(event_loop, code, key_state.is_pressed()),
             _ => {}
         }
     }
